3cd5d6e07e625f605029083a7fed9e54cb8645c4
Andrew Newton <andy@hxr.us>
2019-10-05 13:29:09 -0400

    having done the tutorial

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Fetch from "./Fetch.svelte";
+	import Form from "./Form.svelte";
+
+	let searchTerm;
+
+	function handleSubmit() {
+		const { value } = this.elements.search;
+		searchTerm = value;
+	}
+</script>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
+
+<Form {handleSubmit} />
+<Fetch {searchTerm} let:data>
+	<h1>A list of links</h1>
+	<ul>
+		{#each data as link}
+			<li>
+				<a href={link.url}>{link.title}</a>
+			</li>
+		{/each}
+	</ul>
+</Fetch>
